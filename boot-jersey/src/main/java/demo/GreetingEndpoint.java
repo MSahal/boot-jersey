e6cd75d7a5a057d8ee9bd8befdc1411d4990a4a3
msahal <sahal.mohamed.enis@gmail.com>
2016-02-28 21:37:18 +0100

    comments

--- boot-jersey/src/main/java/demo/GreetingEndpoint.java
+++ boot-jersey/src/main/java/demo/GreetingEndpoint.java
@@ -4,34 +4,36 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+/**
+ * 
+ * @author MSA
+ *
+ */
 @Path("/greetings")
 public class GreetingEndpoint {
-    @Produces({"application/json", "application/xml"})
-    @GET
-    public Message message() {
-        return new Message("Hello world from a Jersey-powered endpoint!");
-    }
+	@Produces({ "application/json", "application/xml" })
+	@GET
+	public Message message() {
+		return new Message("Hello world from a Jersey-powered endpoint!");
+	}
 }
 
 class Message {
-    private String message;
+	private String message;
 
-    public String getMessage() {
-        return message;
-    }
+	public String getMessage() {
+		return message;
+	}
 
-    @Override
-    public String toString() {
-        return "Message{" +
-                "message='" + message + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Message{" + "message='" + message + '\'' + '}';
+	}
 
-    public Message() {
-    }
+	public Message() {
+	}
 
-    Message(String message) {
-        this.message = message;
-    }
+	Message(String message) {
+		this.message = message;
+	}
 }
-
